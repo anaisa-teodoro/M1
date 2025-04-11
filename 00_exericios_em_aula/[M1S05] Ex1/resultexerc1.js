6e74708a242c4c6666616609dbb70050dfcfda6f
anaisa-teodoro <anaisa_teodoro@estudante.sesisenai.org.br>
2025-04-10 23:37:43 -0300

    adicionado exercícios S05

--- 00_exericios_em_aula/[M1S05] Ex1/resultexerc1.js
+++ 00_exericios_em_aula/[M1S05] Ex1/resultexerc1.js
@@ -1,20 +1,19 @@
-// receita_pudim_de_leite.json
-// Exemplo de arquivo JSON
+// Em desenvolvimento..
 import dados from "./receita_pudim_de_leite.json" with { type: "json" };
 
-// Atualiza o título
+
 document.querySelector('h1').textContent = dados.nome;
 
-// Atualiza a lista <ul> com ingredientes
+
 const ul = document.getElementById('ulitem1');
-ul.textContent = ''; // limpa o conteúdo inicial
+ul.textContent = ''; 
 dados.ingredientes.forEach(item => {
   const li = document.createElement('li');
   li.textContent = item;
   ul.appendChild(li);
 });
 
-// Atualiza a lista <ol> com instruções
+
 const ol = document.getElementById('olitem1');
 ol.textContent = '';
 dados.instrucoes.forEach(passo => {
@@ -25,7 +24,7 @@ dados.instrucoes.forEach(passo => {
 
 
 
-// Adiciona imagem ao final
+
 const img = document.createElement('img');
 img.src = dados.imagem;
 img.alt = dados.nome;
